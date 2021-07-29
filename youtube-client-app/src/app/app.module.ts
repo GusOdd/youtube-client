1895f1f13c2655f02c1994c9416f43bdaaf4abd0
GusOdd <gus@dynama.by>
2021-07-29 23:01:45 +0300

    init: start comp-dir-pipes task, add angular material

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +43,7 @@ import { ItemMoreButtonComponent } from './search-results/search-result-item/ite
     ItemVideoTitleComponent,
     ItemMoreButtonComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
