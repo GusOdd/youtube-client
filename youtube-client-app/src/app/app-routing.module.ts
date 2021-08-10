2b687ff5e0158d0dc3160e9e59fbb289e40fb5cd
GusOdd <gus@dynama.by>
2021-08-10 22:38:31 +0300

    feat: add start page and 404 routing

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { PageNotFoundComponent } from './core/pages/page-not-found/page-not-found.component';
+import { StartPageComponent } from './core/pages/start-page/start-page.component';
+
 const routes: Routes = [
+  {
+    path: '',
+    component: StartPageComponent,
+  },
   {
     path: 'results',
     loadChildren: () => import('./youtube/youtube.module').then((m) => m.YoutubeModule),
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+    pathMatch: 'full',
   },
 ];
 
--- youtube-client-app/src/app/core/core.module.ts
+++ youtube-client-app/src/app/core/core.module.ts
@@ -13,6 +13,8 @@ import { LogoComponent } from './components/header/logo/logo.component';
 import { SearchInputComponent } from './components/header/search-input/search-input.component';
 import { SettingsButtonComponent } from './components/header/settings-button/settings-button.component';
 import { LoginInformationComponent } from './components/header/login-information/login-information.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { LoginInformationComponent } from './components/header/login-information
     SearchInputComponent,
     SettingsButtonComponent,
     LoginInformationComponent,
+    PageNotFoundComponent,
+    StartPageComponent,
   ],
   imports: [
     CommonModule,
